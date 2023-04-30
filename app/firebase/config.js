ee3eda7c2af1f3dc565dc8405361515249a610dc
ben.phoebus@speedymoney.com.au <ben.phoebus@speedymoney.com.au>
2023-04-30 20:01:44 +1000

    firebase config ios - app ios build success

---
+++ app/firebase/config.js
@@ -0,0 +1,33 @@
+// https://console.firebase.google.com/project/moneyspot-app/settings/general/ios:com.moneyspot.app.ios
+// Add initialization code
+// To connect Firebase when your app starts up, add the initialization code below to your app's main entry point.
+
+// SwiftUI
+
+// import SwiftUI
+// import FirebaseCore
+
+
+// class AppDelegate: NSObject, UIApplicationDelegate {
+//   func application(_ application: UIApplication,
+//                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//     FirebaseApp.configure()
+
+//     return true
+//   }
+// }
+
+// @main
+// struct YourApp: App {
+//   // register app delegate for Firebase setup
+//   @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+//   var body: some Scene {
+//     WindowGroup {
+//       NavigationView {
+//         ContentView()
+//       }
+//     }
+//   }
+// }
